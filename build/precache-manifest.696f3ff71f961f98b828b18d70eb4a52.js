9f9b3f76d929a7e7ba90fc7d755cbd8d7818d356
Sidharth kumar Pradhan <131594785+Igrish990@users.noreply.github.com>
2025-10-24 10:23:07 +0000

    Update

---
+++ build/precache-manifest.696f3ff71f961f98b828b18d70eb4a52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0bb11f180be6a135eecdd7463ecafd9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc3f61e7bcb786e4ceeb",
+    "url": "/static/css/main.1d40fa6d.chunk.css"
+  },
+  {
+    "revision": "afa9b2055ae5ce5c3c4e",
+    "url": "/static/js/2.8692939f.chunk.js"
+  },
+  {
+    "revision": "d0f416c0b71d0ec4a4b3180b59613874",
+    "url": "/static/js/2.8692939f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc3f61e7bcb786e4ceeb",
+    "url": "/static/js/main.8da96939.chunk.js"
+  },
+  {
+    "revision": "0fb5aa10d11e0a3e8d13",
+    "url": "/static/js/runtime-main.56c06abf.js"
+  }
+]);
